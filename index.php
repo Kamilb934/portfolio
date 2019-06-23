54d1acd72f0da74d780d23f931dc4947fa5e9c77
Kamilb934 <kamilb934@gmail.com>
2019-06-23 21:31:20 +0200

    rewizja poczatkowa

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Kamil Buczyński</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css\fontello\css\fontello.css">
+  <link href="https://fonts.googleapis.com/css?family=B612+Mono|Cuprum|Lato&display=swap" rel="stylesheet">
+</head>
+<body>
+  <div class="wrapper">
+   <div class="container">
+        <header><div class="logo">//Kamil </div><div class="logo2">Buczyński</div><div class="blink">_</div></header>
+        
+        
+        <div class="content">
+          <nav>
+           <div id="important">
+               <a href="#"><div class="icon" id="skills"><i class="icon-cog-alt"></i>Skills</div></a>
+               <a href="contact"><div class="icon" id="contact"><i class="icon-mail"></i>Contact</div></a>
+               <a href="projects"><div class="icon" id="projects"><i class="icon-terminal"></i>Projects</div></a>
+               <a href="cv"><div class="icon" id="cv"><i class="icon-doc-text-inv"></i>CV</div></a>
+            </div></nav>
+            
+            <div class="about">
+            <div class="quote">The human spirit must prevail over technology.</div>
+            </div> 
+            <div id="important2">
+            <a href="github"><div class="icon2" id="github"><i class="icon-github-circled"></i>Github</div></a>
+            <a href="linkedin"><div class="icon2" id="linkedin"><i class="icon-linkedin-rect-1"></i>Linkedin</div></a>
+            <a href="stackoverflow"><div class="icon2" id="stackoverflow"><i class="icon-stackoverflow"></i>Stack<br/>Overflow</div></a>
+            </div>
+           
+        </div>
+        
+
+    </div>
+    
+    </div>
+</body>
+</html>
